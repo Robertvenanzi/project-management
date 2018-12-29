e813da0a1da830f6a5d90bbe588421d6cac14ec4
robertvenanzi <robertgvenanzi@gmail.com>
2018-12-29 15:25:47 -0800

    added server

--- enamel/server/src/app.js
+++ enamel/server/src/app.js
@@ -1,3 +1,5 @@
+
+
 const { GraphQLServer } = require('graphql-yoga')
 const mongoose = require('mongoose')
 require('dotenv').config()
@@ -9,18 +11,15 @@ db.on("error", console.error.bind(console, "connection error"))
 db.once("open", function (callback) {
     console.log("Connection Succeeded")
 })
-
 const server = new GraphQLServer({
     typeDefs: 'src/schema.graphql',
     resolvers,
     context: req => req,
 })
-
 const options = {
     port: process.env.PORT || 5500,
     endpoint: '/graphql',
     subscriptions: '/subscriptions',
     playground: '/playground',
 }
-
 server.start(options, ({ port }) => console.log(`Server is running on port ${port}`))
